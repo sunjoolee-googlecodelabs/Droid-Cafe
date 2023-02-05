3498f9d10b3c4385cf820058f9e53259ea1a32f5
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2023-02-05 15:54:13 +0900

    5. Task 3: Change the floating action button

--- app/src/main/java/googlecodelabs/droidcafe/MainActivity.kt
+++ app/src/main/java/googlecodelabs/droidcafe/MainActivity.kt
@@ -1,11 +1,40 @@
 package googlecodelabs.droidcafe
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fab = findViewById<FloatingActionButton>(R.id.fab)
+        fab.apply{
+            setImageResource(R.drawable.ic_shopping_cart)
+            setOnClickListener {
+                val intent = Intent(context, OrderActivity::class.java)
+                startActivity(intent)
+            }
+        }
+    }
+
+    fun showDonutOrder(view: View) {
+        displayToast(getString(R.string.donut_order_message))
+    }
+    fun showIceCreamOrder(view: View) {
+        displayToast(getString(R.string.ice_cream_order_message))
+    }
+    fun showFroyoOrder(view: View) {
+        displayToast(getString(R.string.froyo_order_message))
+    }
+
+    private fun displayToast(message: String) {
+        Toast.makeText(applicationContext, message, Toast.LENGTH_SHORT).show()
     }
 }

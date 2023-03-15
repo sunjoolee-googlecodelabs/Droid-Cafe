8d44a0f36baa1a63ca57d335264750bcc56c1ff8
sunjoolee <spottypoo124@gmail.com>
2023-03-15 15:40:22 +0900

    3. Task 1: Add items to the options menu
    1.1 Examine the code
    (add menu)

--- app/src/main/java/googlecodelabs/droidcafe/MainActivity.kt
+++ app/src/main/java/googlecodelabs/droidcafe/MainActivity.kt
@@ -2,12 +2,16 @@ package googlecodelabs.droidcafe
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import googlecodelabs.droidcafe.databinding.ActivityMainBinding
 
 const val EXTRA_ORDER_MESSAGE :String = "googlecodelabs.droidcafe.extra.ORDER_MESSAGE"
 
@@ -15,23 +19,19 @@ class MainActivity : AppCompatActivity() {
 
     private var mOrderMessage : String = ""
 
-    private lateinit var donutImageView : ImageView
-    private lateinit var icecreamImageView : ImageView
-    private lateinit var froyoImageView : ImageView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        donutImageView = findViewById(R.id.donut)
-        icecreamImageView = findViewById(R.id.ice_cream)
-        froyoImageView = findViewById(R.id.froyo)
+        val donutImageView = findViewById<ImageView>(R.id.donut)
+        val iceCreamImageView = findViewById<ImageView>(R.id.ice_cream)
+        val froyoImageView = findViewById<ImageView>(R.id.froyo)
 
         donutImageView.setOnClickListener{
             mOrderMessage = getString(R.string.donut_order_message)
             displayToast(mOrderMessage)
         }
-        icecreamImageView.setOnClickListener{
+        iceCreamImageView.setOnClickListener{
             mOrderMessage = getString(R.string.ice_cream_order_message)
             displayToast(mOrderMessage)
         }
@@ -53,8 +53,21 @@ class MainActivity : AppCompatActivity() {
                 startActivity(intent)
             }
         }
-    }
 
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.app_bar_menu, menu)
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.action_settings -> {
+                Toast.makeText(this, "settings item clicked", Toast.LENGTH_SHORT).show()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
 
